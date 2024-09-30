b0e40c3e459c2fdc531916572a691055398be13d
Haryce <148765923+Haryce@users.noreply.github.com>
2024-09-30 14:53:52 +0900

    Add files via upload

---
+++ 1i.cpp
@@ -0,0 +1,22 @@
+﻿#include <iostream>
+
+using namespace std;
+
+int main() {
+	setlocale(LC_ALL, "Russian");
+	int N, K; // Количество белок и орехов
+
+	// Ввод данных
+	cout << "Введите количество белок (N): ";
+	cin >> N;
+	cout << "Введите количество орехов (K): ";
+	cin >> K;
+
+	// Вычисление остатка
+	int ost = K % N;
+
+	// Вывод результата
+	cout << "Останется " << ost << " орехов." << endl;
+
+	return 0;
+}
